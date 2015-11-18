1e6d4fe4aa3ab35212cf1e992bad48c66f290491
ghorreso <omid.ghorreshi@ensimag.grenoble-inp.fr>
2015-11-18 13:33:01 +0100

    Physics tests
    A scene to test the control of the character with the wind

---
+++ FallSeason/Assets/Scripts/Restart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Restart : MonoBehaviour {
+
+	public float xInitial;
+	public float yInitial;
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			transform.position = new Vector2 (xInitial, yInitial);
+		}
+	}
+}
