43c3f3f55cad24a67b8a39d43daaa20de33facc5
Mikael <mikael.sven.eriksson@gmail.com>
2015-11-25 14:16:26 +0100

    Added character animation scene

--- FallSeason/Assets/Scripts/UmbrellaFolding.cs
+++ FallSeason/Assets/Scripts/UmbrellaFolding.cs
@@ -5,31 +5,19 @@ public class UmbrellaFolding : MonoBehaviour {
 
 	public float gravityMult;
 
-	private bool umbrellaFolded = false;
-	private float doubleClickStart = 0.0f;
-
-	void OnMouseUp()
-	{
-		if ((Time.time - doubleClickStart) < 0.3f)
-		{
-			this.OnDoubleClick();
-			doubleClickStart = -1.0f;
-		}
-		else
-		{
-			doubleClickStart = Time.time;
-		}
-	}
-
-	void OnDoubleClick () {
-		if (!umbrellaFolded) {
-			umbrellaFolded = true;
-			this.transform.GetComponent<Rigidbody2D> ().gravityScale *= gravityMult;
-			this.GetComponent<SpriteRenderer> ().color = new Color (1, 0, 0, 1);
-		} else {
-			umbrellaFolded = false;
-			this.transform.GetComponent<Rigidbody2D> ().gravityScale /= gravityMult;
-			this.GetComponent<SpriteRenderer> ().color = new Color (0, 1, 0, 1);
+	private bool mousePressed = false;
+	void Update () {
+		Debug.Log(this.transform.GetComponent<Rigidbody2D> ().gravityScale);
+		if (Input.GetMouseButtonDown (0)) {
+			if (!mousePressed) {
+				mousePressed = true;
+				this.transform.GetComponent<Rigidbody2D> ().gravityScale *= gravityMult;
+				this.GetComponent<SpriteRenderer> ().color = new Color (1, 0, 0, 1);
+			} else {
+				mousePressed = false;
+				this.transform.GetComponent<Rigidbody2D> ().gravityScale /= gravityMult;
+				this.GetComponent<SpriteRenderer> ().color = new Color (0, 1, 0, 1);
+			}
 		}
 	}
 }
