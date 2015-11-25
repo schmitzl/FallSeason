9191ef30c74453b9dca31831c59a3b7edf6f261c
ghorreso <omid.ghorreshi@ensimag.grenoble-inp.fr>
2015-11-25 12:45:48 +0100

    Picking coins and umbrella folding

---
+++ FallSeason/Assets/Scripts/UmbrellaFolding.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UmbrellaFolding : MonoBehaviour {
+
+	public float gravityMult;
+
+	private bool mousePressed = false;
+	void Update () {
+		Debug.Log(this.transform.GetComponent<Rigidbody2D> ().gravityScale);
+		if (Input.GetMouseButtonDown (0)) {
+			if (!mousePressed) {
+				mousePressed = true;
+				this.transform.GetComponent<Rigidbody2D> ().gravityScale *= gravityMult;
+				this.GetComponent<SpriteRenderer> ().color = new Color (1, 0, 0, 1);
+			} else {
+				mousePressed = false;
+				this.transform.GetComponent<Rigidbody2D> ().gravityScale /= gravityMult;
+				this.GetComponent<SpriteRenderer> ().color = new Color (0, 1, 0, 1);
+			}
+		}
+	}
+}
