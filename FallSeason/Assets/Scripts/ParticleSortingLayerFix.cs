f963dc01374cc35fbdcbf6ef3bf7f78e80be2627
Mikael Eriksson <aldur92@hotmail.com>
2015-11-26 21:40:04 +0100

    Made script for dragon movement, collider

---
+++ FallSeason/Assets/Scripts/ParticleSortingLayerFix.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSortingLayerFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = "Foreground";
+		GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingOrder = 4; //dont display on top of jetpack
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
