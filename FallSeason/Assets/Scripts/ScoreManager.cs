bd8650baee9fd5ec5dd6583c66c2cb83f55a9bb3
lemoele <elemoel@n129-p65.kthopen.kth.se>
2015-11-30 14:06:52 +0100

    Modification of the score depending on the speed.

--- FallSeason/Assets/Scripts/ScoreManager.cs
+++ FallSeason/Assets/Scripts/ScoreManager.cs
@@ -8,9 +8,14 @@ public class ScoreManager : MonoBehaviour {
 	public Text distanceText;
 	public Text coinText;
 
-	private int score;
+	private int prevDistance;
+	private int prevCoins;
+
 	private int distance;
+	private float speed;
+	private int speedCombo;
 	private int coins;
+	private int score;
 
 	// Use this for initialization
 	void Start () {
@@ -25,9 +30,28 @@ public class ScoreManager : MonoBehaviour {
 	}
 
 	void UpdateValues () {
+		// Update the distance
+		prevDistance = distance;
 		distance = GetComponent<ScrollingManager>().distance;
+
+		// Update the speed combo
+		speed = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y);
+		if (speed < 0.01) {
+			speed = GetComponent<ScrollingManager>().speed;
+			speedCombo = (int) (speed * 30);
+		} else {
+			speedCombo = (int) (speed);
+		}
+		if (speedCombo == 0) {
+			speedCombo++;
+		}
+
+		// Udpate the number of coins
+		prevCoins = coins;
 		coins = GetComponent<PickCoins>().coins;
-		score = distance * 10 + coins;
+
+		// Update the score
+		score += (distance - prevDistance) * speedCombo * 10 + (coins - prevCoins);
 	}
 
 	void UpdateTexts () {
