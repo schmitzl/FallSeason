53324f5ebd7fd8edd67cda3b8fbfe0bde9456939
lemoele <elemoel@n129-p65.kthopen.kth.se>
2015-11-30 10:47:40 +0100

    Integrated the correction to the main scene.

--- FallSeason/Assets/Scripts/MoveCoin.cs
+++ FallSeason/Assets/Scripts/MoveCoin.cs
@@ -3,18 +3,16 @@ using System.Collections;
 
 public class MoveCoin : MonoBehaviour {
 
-	private GameObject mainCamera;
 	private GameObject character;
 
 	void Start() {
-		mainCamera = GameObject.Find ("Main Camera");
 		character = GameObject.Find ("Character");
 	}
 
 	void Update () {
 		float speed;
 		if (character.GetComponent<Rigidbody2D> ().velocity.y == 0) {
-			speed = mainCamera.GetComponent<ScrollingManager> ().speed;
+			speed = character.GetComponent<ScrollingManager> ().speed;
 			this.transform.position += new Vector3(0,speed,0);
 		}
 	}
