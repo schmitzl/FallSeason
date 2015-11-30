3f078299402875167bf21210661aecaac518347f
lemoele <elemoel@n129-p65.kthopen.kth.se>
2015-11-30 13:21:48 +0100

    Created basic scoring system.
    DONE :
    - Get the distance from the scrolling manager.
    - Get the number of coins from the pick coins script, depending on the
    value of the coin.
    - Calculate the score as 10 times distance to which we add the number
    of coins.
    - Display the 3 scoring items in the game
    
    TODO :
    - Integrate in main scene (canvas prefab).
    - Create prefabs for coins with the correct tags.
    - Create nice UI for the different scores.
    - Modify score depending on speed (bonus).

---
+++ FallSeason/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+	
+	public Text scoreText;
+	public Text distanceText;
+	public Text coinText;
+
+	private int score;
+	private int distance;
+	private int coins;
+
+	// Use this for initialization
+	void Start () {
+		score = distance = coins = 0;
+		UpdateTexts();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateValues();
+		UpdateTexts();
+	}
+
+	void UpdateValues () {
+		distance = GetComponent<ScrollingManager>().distance;
+		coins = GetComponent<PickCoins>().coins;
+		score = distance * 10 + coins;
+	}
+
+	void UpdateTexts () {
+		scoreText.text 		= "Score : " 	+ score.ToString ();
+		distanceText.text 	= "Distance : " + distance.ToString ();
+		coinText.text 		= "Coins : " 	+ coins.ToString ();
+	}
+}
