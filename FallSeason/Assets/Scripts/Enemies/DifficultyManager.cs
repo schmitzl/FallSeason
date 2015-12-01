cd9fb1885db8f30fd69f6b932a400d0bf64c7041
Mikael Eriksson <aldur92@hotmail.com>
2015-12-01 20:59:04 +0100

    Added simple DifficultyManager.

---
+++ FallSeason/Assets/Scripts/Enemies/DifficultyManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DifficultyManager : MonoBehaviour {
+
+	public GameObject dragonEnemy;
+
+	public void spawn(GameObject level){
+
+		SpriteRenderer spriteRenderer = level.GetComponent<SpriteRenderer>();
+		Vector3 max = spriteRenderer.bounds.max;
+		Vector3 center = spriteRenderer.bounds.center;
+
+		GameObject dragon = (GameObject) Instantiate(dragonEnemy, new Vector3(max.x, center.y, center.z), Quaternion.identity);
+		dragon.transform.SetParent(level.transform);
+	}
+}
