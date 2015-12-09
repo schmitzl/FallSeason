cc832ee6da26afce4181c4f2c15c7a1443deb89a
Mikael <mikael.sven.eriksson@gmail.com>
2015-12-09 17:43:26 +0100

    Added trailer scene.

---
+++ FallSeason/Assets/Scripts/EnterMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterMenu : MonoBehaviour {
+
+	void Update() {
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel(1);
+		}
+	}
+}
