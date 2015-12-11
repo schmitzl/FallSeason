dcc8e605e9a68d63bd73e9330c2d9f64f30e1f7f
schmitzl <lisa.schmitz@student.uni-tuebingen.de>
2015-12-11 10:36:51 +0100

    added delay scene and icon

---
+++ FallSeason/Assets/Scripts/DelayScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayScript : MonoBehaviour {
+
+	float delay = 3;
+	
+	
+	void Update()
+	{
+		delay -= Time.deltaTime;
+		if (delay <= 0)
+			Application.LoadLevel("Trailer1");
+	}
+}
